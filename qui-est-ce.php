261097026bfb2c88a113b311deba0cf513cb6e05
22303857 <kalambay.Dinanga@gmail.com>
2024-11-25 17:32:39 +0100

    avancer du tableau

--- qui-est-ce.php
+++ qui-est-ce.php
@@ -4,10 +4,9 @@
 function liste(){
     $csv=fopen("joueurs.csv","r");
     fgetcsv($csv);
-    echo "<div id='liste_carte'><table>";
+    echo "<div id='liste_carte'><table id='tableau'>";
 
-    for ($i=0;$i<5;$i++) {
-        $perso=fgetcsv($csv);
+    while ($perso=fgetcsv($csv)) {
 
         echo "<tr><td><div id='carte'><img src='images/personnages/".$perso[0]."' alt='photo' id='photo_carte'>";
         echo "<p>".$perso[1]."</p></div></td></tr>";
@@ -15,12 +14,7 @@ function liste(){
 
     echo "</table></div>";
 }
-if (isset($_POST['guess'])){
-    $mot = $_POST['guess'];
-}
-function essai(){
-    echo 'oui';
-}
+
 
 echo "<head>
         <meta charset='utf-8'>
@@ -33,10 +27,18 @@ echo "<div>
        </div>";
 
 echo "<form method='post' action='' id='formulaire'>
-        <input type='text' name='guess' id='guess' placeholder='Nom de personnage, alias' onkeyup='essai()'>
+        <input type='text' name='guess' id='guess' placeholder='Nom de personnage, alias' onkeyup='ok()'>
         <input type='submit' value='Envoyer'>
        </form>
+       <p id='caca'></p>
         ";
+liste();
+echo "<script>
+    function ok(){
+        var caca = document.getElementById('caca');
+        caca.innerHTML = document.getElementById('guess').value;
+    }
+</script>";
 
 
 
